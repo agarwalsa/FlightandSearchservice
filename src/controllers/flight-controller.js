3b544b9e484c9fa9b1fb5ba2856d054949206f9f
SAMARPIT AGARWAL <agarwalsamarpit@gmail.com>
2024-03-08 18:37:31 +0530

    added error code enums in the utils file

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,4 +1,5 @@
 const {FlightService} = require('../services/index');
+const {SuccessCodes} = require('../utils/error-codes');
 const flightService=new FlightService();
 const create = async (req,res) => {
     try{
@@ -13,7 +14,7 @@ const create = async (req,res) => {
         departureTime:  req.body.departureTime
       }
    const flight =  await flightService.createFlight(FlightRequestData);
-   return res.status(201).json({
+   return res.status(SuccessCodes.CREATED).json({
             data:flight,
             success:true,
             
@@ -33,7 +34,7 @@ const create = async (req,res) => {
 const getAll = async (req,res) => {
     try{
     const response = await flightService.getAllFlightData(req.query);
-    return res.status(200).json({
+    return res.status(SuccessCodes.OK).json({
         data:response,
         success:true,
          message:"able to fetch  the flights in a successful manner here",
